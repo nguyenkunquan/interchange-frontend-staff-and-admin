2cc512771168e0c22309bdc75515d361104107e1
nguyenkunquan <nguyenkunquan@gmail.com>
2024-03-18 02:08:43 +0700

    first commit

---
+++ src/main/java/com/nguyenkunquan/interchangefrontendnguyenkunquan/InterchangeFrontendNguyenkunquanApplication.java
@@ -0,0 +1,13 @@
+package com.nguyenkunquan.interchangefrontendnguyenkunquan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterchangeFrontendNguyenkunquanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InterchangeFrontendNguyenkunquanApplication.class, args);
+    }
+
+}
